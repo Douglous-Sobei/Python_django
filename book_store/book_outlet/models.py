8f0b63996f43ce6c0978b61cbe16b8b8d5329371
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-25 06:41:18 +0300

    created new model called Address

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,9 +4,15 @@ from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
+class Address(models.Model):
+    street = models.CharField(max_length=70)
+    postal_code = models.CharField(max_length=6)
+    city = models.CharField(max_length=30)
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
