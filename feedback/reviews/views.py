9d96d8edec30b8941bc820ee3ad44eb08d9058b1
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-28 23:22:41 +0300

    update

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def review(request):
-    return render(request, "reviews/review.html")
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+
+        if entered_username == "":
+            return render(request, "reviews/review.html", {
+                "has_error": True
+            })
+        print(entered_username)
+        return HttpResponseRedirect("/thank-you")
+    
+    return render(request, "reviews/review.html", {
+        "has_error": False
+    })
+
+def thank_you(request):
+    return render(request, "reviews/thanks.html")
