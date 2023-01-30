8ecfcd4aff8755ceda01eee54d2d9c36a2ef9e9c
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-30 20:11:02 +0300

    Template views

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 from .models import Review
-
 
 
 # Create your views here.
@@ -27,17 +27,10 @@ class MyView(View):
         "form": form
     })
 
-def review(request):
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-    else:
-        form = ReviewForm()
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+class ThankYouView(TemplateView):
+    template_name = "reviews/thanks.html"
 
-def thank_you(request):
-    return render(request, "reviews/thanks.html")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This is workable!"
+        return context
