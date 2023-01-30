72b49e450ce4228176a8b6ad051d6d51ea3757b6
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-30 21:25:01 +0300

    The list views

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .forms import ReviewForm
 from .models import Review
@@ -35,14 +36,15 @@ class ThankYouView(TemplateView):
         context["message"] = "This is workable!"
         return context
     
-class ReviewListView(TemplateView):
+class ReviewListView(ListView):
     template_name = "reviews/review-list.html"
+    model = Review
+    context_object_name = "reviews"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=2)
+    #     return data
 
 class SingleReviewView(TemplateView):
     template_name = "reviews/single-review.html"
