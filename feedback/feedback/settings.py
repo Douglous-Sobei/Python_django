0115bd3034e0377b04b19806af0fe4a415b02b9c
Douglous Sobei <douglousmangoyi@gmail.com>
2023-02-01 02:41:46 +0300

    Using models for File storage

--- feedback/feedback/settings.py
+++ feedback/feedback/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_ROOT = BASE_DIR / "uploads"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
