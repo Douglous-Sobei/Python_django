b3a9d0cfb088c13ae041d39711a45dcb99186dbc
Douglous Sobei <douglousmangoyi@gmail.com>
2023-02-01 06:57:01 +0300

    Which kind of data should be stored

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -42,6 +42,5 @@ class SingleReviewView(DetailView):
 class AddFavoriteView(View):
     def post(self, request):
         review_id = request.POST['review_id']
-        favorite_review = Review.objects.get(pk=review_id)
-        request.session["favorite_review"] = favorite_review
+        request.session["favorite_review"] = review_id
         return HttpResponseRedirect("/reviews/" + review_id)
