6f20a2c84bd9e82751330ec16eea323c93bf3b9d
Douglous Sobei <douglousmangoyi@gmail.com>
2023-02-07 00:22:44 +0300

    Using enviroment variables

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-e57va)xn%!=vmdfv)6)8j8=e21&plz3(4p(jni@^&-(n#d-ki3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = getenv("IS_PRODUCTION", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("MY_HOST")
+]
 
 
 # Application definition
