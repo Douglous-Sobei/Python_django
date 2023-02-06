e736a6f138e77851c05e7ec9c70d3bff8a26f552
Douglous Sobei <douglousmangoyi@gmail.com>
2023-02-06 22:40:50 +0300

    collecting my static files

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-e57va)xn%!=vmdfv)6)8j8=e21&plz3(4p(jni@^&-(n#d-ki3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
